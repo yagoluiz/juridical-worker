079f280aabae89a43b3cd93bf659c694ddbb2c13
Yago Luiz <yago.luiz@hotmail.com>
2022-02-14 23:33:36 -0300

    Refactor deploy paths

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,10 +1,12 @@
+# /bin/bash
+
 PATH_DEFAULT=k8s
 
-kubectl apply -f $PATH_DEFAULT/juridical-namespace.yaml
+kubectl apply -f $PATH_DEFAULT/namespaces/juridical-namespace.yaml
 
 kubectl create secret generic juridical-worker-secret -n juridical --from-env-file=.env
 
-kubectl apply -f $PATH_DEFAULT/juridical-selenium-deployment.yaml
-kubectl apply -f $PATH_DEFAULT/juridical-worker-deployment.yaml
+kubectl apply -f $PATH_DEFAULT/deployments/juridical-selenium-deployment.yaml
+kubectl apply -f $PATH_DEFAULT/deployments/juridical-worker-deployment.yaml
 
-kubectl apply -f $PATH_DEFAULT/juridical-selenium-service.yaml
+kubectl apply -f $PATH_DEFAULT/services/juridical-selenium-service.yaml
