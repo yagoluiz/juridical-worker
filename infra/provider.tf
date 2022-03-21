fcdb7866c9e60313d3d1ef443ac85928d72b4c13
Yago Luiz <yago.luiz@hotmail.com>
2022-03-21 15:39:21 -0300

    Comment provider credentials test

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project     = var.project_id
-  region      = var.region
-  credentials = file(var.credentials_file)
+  project = var.project_id
+  region  = var.region
+  # credentials = file(var.credentials_file)
 }
