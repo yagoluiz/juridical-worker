31b6effbffb14c98651173f5c7153af87d67471c
Yago Luiz <yago.luiz@hotmail.com>
2022-08-24 15:02:00 -0300

    Refactor container setup

--- src/Juridical.LegalProcess.Worker/Program.cs
+++ src/Juridical.LegalProcess.Worker/Program.cs
@@ -1,17 +1,9 @@
-using Juridical.Core.Interfaces;
-using Juridical.Core.Services;
 using Juridical.LegalProcess.Worker.Workers;
 
 var host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((context, services) =>
+    .ConfigureServices((_, services) =>
     {
         services.AddHostedService<LegalProcessWorker>();
-        services.AddHttpClient<IMessageService, MessageService>(client =>
-        {
-            client.BaseAddress = new Uri(context.Configuration.GetValue<string>("MESSAGE_SERVICE_URL"));
-            client.DefaultRequestHeaders.Add("x-api-token",
-                context.Configuration.GetValue<string>("MESSAGE_SERVICE_API_TOKEN"));
-        });
         services.AddMemoryCache();
     })
     .Build();
--- src/Juridical.Message.Worker/Program.cs
+++ src/Juridical.Message.Worker/Program.cs
@@ -1,8 +1,18 @@
-using Juridical.Message.Worker;
+using Juridical.Core.Interfaces;
+using Juridical.Core.Services;
 using Juridical.Message.Worker.Workers;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => { services.AddHostedService<MessageWorker>(); })
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddHostedService<MessageWorker>();
+        services.AddHttpClient<IMessageService, MessageService>(client =>
+        {
+            client.BaseAddress = new Uri(context.Configuration.GetValue<string>("MESSAGE_SERVICE_URL"));
+            client.DefaultRequestHeaders.Add("x-api-token",
+                context.Configuration.GetValue<string>("MESSAGE_SERVICE_API_TOKEN"));
+        });
+    })
     .Build();
 
 await host.RunAsync();
