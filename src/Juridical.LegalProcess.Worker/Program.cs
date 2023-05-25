45f3868e25ec481f3fb94c92f016728f7c54c136
Yago Luiz <yago.luiz@hotmail.com>
2023-05-25 18:19:23 -0300

    Add health check config

--- src/Juridical.LegalProcess.Worker/Program.cs
+++ src/Juridical.LegalProcess.Worker/Program.cs
@@ -1,3 +1,4 @@
+using Juridical.Core.HealthChecks;
 using Juridical.Core.Interfaces;
 using Juridical.Core.Services;
 using Juridical.LegalProcess.Worker.Workers;
@@ -8,6 +9,7 @@ var host = Host.CreateDefaultBuilder(args)
         services.AddSingleton<IPublisherService, GooglePubSubPublisherService>();
         services.AddMemoryCache();
         services.AddHostedService<LegalProcessWorker>();
+        services.AddHostedService<HealthCheckWorker>();
     })
     .Build();
 
