b25d140ac92ef6fc9d94fd0692746d73fe76a936
Yago Luiz <yago.luiz@hotmail.com>
2023-10-16 12:12:45 -0300

    Fix infra destroy

--- infra/gke.tf
+++ infra/gke.tf
@@ -4,6 +4,8 @@ resource "google_container_cluster" "juridical-cluster" {
 
   remove_default_node_pool = true
   initial_node_count       = 1
+
+  deletion_protection = false
 }
 
 resource "google_container_node_pool" "juridical-node-pool" {
